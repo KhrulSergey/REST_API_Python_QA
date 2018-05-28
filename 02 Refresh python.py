15e98acc25c38191df281e7cbadb7fa691bb0c76
Sergey Chrul <siberianodis@gmail.com>
2018-05-28 11:03:58 +0200

    Initial commit

---
+++ 02 Refresh python.py
@@ -0,0 +1,37 @@
+__author__ = 'Sergey Khrul'
+
+
+# First task of Section #2
+var1 = "test"
+var2 = "test"
+print(var1, " and ", var2)
+
+num1 = 8
+num2 = 2
+
+print("If ", num1, " multiply by ", num2, " the answer will = ", num1*num2)
+
+# Second task of Section #2
+def return_42 ():
+    return 42
+
+def my_method(num1: float, num2: float):
+    return num1*num2
+
+# Second task of Section #2
+
+
+my_list = [35, 45, 20]
+print("my list sum = ", sum(my_list))
+
+my_tuple = (1, )
+print("my tuple = ", my_tuple)
+
+set1 = {14, 5, 9, 31, 12, 77, 67, 8}
+set2 = {5, 77, 9, 12}
+
+# should returns {5, 77, 9, 12}
+print("intersections of sets is ", set1.intersection(set2))
+
+# Third task of Section #2
+
